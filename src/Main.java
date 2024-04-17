89f0659ad9d8f9a7bc3573e61148afa4fe991fcc
edelacruz11 <edelacruz@elpuig.xeill.net>
2024-04-17 20:06:43 +0200

    Metodos auxiliares del menu

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    private void newGame(){}
+    private void loadGame(){}
+    private void settings(){}
+    private void exit(){}
 }
