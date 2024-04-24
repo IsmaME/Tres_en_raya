3074d7eed85f6a624f720a404119054622111ce5
edelacruz11 <edelacruz@elpuig.xeill.net>
2024-04-24 20:00:16 +0200

    Clase TUI y Main actualizadas

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,39 @@
+import jdk.jshell.spi.ExecutionControl;
+
 public class Main {
     public static void main(String[] args) {
         TUI tui = new TUI();
-        tui.showMenu();
-    }
+        int op = tui.showMenu();
 
+        switch (op) {
+            case 1:
+                System.out.printf("Has seleccionat: Nova partida!\n");
+                //newGame();
+                break;
+            case 2:
+                System.out.printf("Has seleccionat: Carregar partida!\n");
+                //loadGame();
+                break;
+            case 3:
+                System.out.printf("Has seleccionat: Configuració\n");
+                //settings();
+                break;
+            case 4:
+                System.out.printf("Has seleccionat: Sortir\n");
+                //exit();
+                break;
+        }
+    }
+    private static void newGame() throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("Por hacer.");
+    }
+    private static void loadGame() throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("Por hacer.");
+    }
+    private static void settings() throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("Por hacer.");
+    }
+    private static void exit() throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("Por hacer.");
+    }
 }
--- src/TUI.java
+++ src/TUI.java
@@ -1,47 +1,43 @@
+import jdk.jshell.spi.ExecutionControl;
+
 import java.util.Scanner;
 
 public class TUI {
-    void showMenu(){
-        Scanner sc = new Scanner(System.in);
+    private Scanner sc = new Scanner(System.in);
 
+    public int showMenu(){
+
+        System.out.print(
+                "Benvingut a Tic Tac Toe!\n" +
+                        "[1] Nova partida\n" +
+                        "[2] Carregar partida\n" +
+                        "[3] Configuració\n" +
+                        "[4] Sortir\n");
+
+        int op = sc.nextInt();
         boolean control = true;
 
         while (control) {
-            System.out.printf(
-                    "Benvingut a Tic Tac Toe!\n" +
-                            "[1] Nova partida\n" +
-                            "[2] Carregar partida\n" +
-                            "[3] Configuració\n" +
-                            "[4] Sortir\n");
-
-            int op = sc.nextInt();
-
-            switch (op) {
-                case 1:
-                    System.out.printf("Has seleccionat: Nova partida!\n");
-                    newGame();
-                    break;
-                case 2:
-                    System.out.printf("Has seleccionat: Carregar partida!\n");
-                    loadGame();
-                    break;
-                case 3:
-                    System.out.printf("Has seleccionat: Configuració\n");
-                    settings();
-                    break;
-                case 4:
-                    System.out.printf("Has seleccionat: Sortir\n");
-                    exit();
-                    control = false;
-                    break;
-                default:
-                    System.out.printf("Numero invàlid!\n");
+            if (op <= 0 || op > 4) {
+                System.out.println("Opcio invalida!");
+                System.out.print(
+                                "[1] Nova partida\n" +
+                                "[2] Carregar partida\n" +
+                                "[3] Configuració\n" +
+                                "[4] Sortir\n");
+            } else {
+                control = false;
             }
         }
-
+        return op;
     }
-    private void newGame(){}
-    private void loadGame(){}
-    private void settings(){}
-    private void exit(){}
+    public void showBoard(char[][] taulell, short torn) throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("Por hacer.");
+    }
+    public short pickUpPlay() throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("Por hacer.");
+    }
+    public void endOfGame(short guanyador) throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("Por hacer.");
+    }
 }
