50507382a91b7c573c3c1695adc0cdd4066ae9d8
ismam <imehdi@elpuig.xeill.net>
2024-04-27 01:52:58 +0200

    Se ha creado la prueba del metodo play y se ha programado el codigo del metodo en cuestion para superar esta prueba

--- src/Joc.java
+++ src/Joc.java
@@ -2,14 +2,26 @@ import jdk.jshell.spi.ExecutionControl;
 
 public class Joc {
     private char[][] board;
-    private short turn;
+    private short turn = 1;
 
     //Methods
     public void newGame() throws ExecutionControl.NotImplementedException {
         throw new ExecutionControl.NotImplementedException("Method newGame not implemented");
     }
-    public void play(short row, short column) throws ExecutionControl.NotImplementedException {
-        throw new ExecutionControl.NotImplementedException("Method play not implemented");
+    public void play(short row, short column)  {
+        board = new char[3][3];
+
+        //add pice X or O and switch the turns
+        switch (turn) {
+            case 1:
+                board[row][column] = 'X';
+                turn = 2;
+                break;
+            case 2:
+                board[row][column] = 'O';
+                turn = 1;
+                break;
+        }
     }
     public boolean winning_play(short row,short column) throws ExecutionControl.NotImplementedException {
         throw new ExecutionControl.NotImplementedException("Method wining_play not implemented");
--- src/TestJoc.java
+++ src/TestJoc.java
@@ -1,3 +1,4 @@
+import jdk.jshell.spi.ExecutionControl;
 import org.junit.jupiter.api.Assertions;
 
 public class TestJoc {
@@ -7,8 +8,37 @@ public class TestJoc {
     }
 
     @org.junit.jupiter.api.Test
-    void newGame() {
+    void play() throws ExecutionControl.NotImplementedException {
+        Joc game1 = new Joc();
+        Joc game2 = new Joc();
 
+        char[][] board_player1 = new char[3][3];
+        char[][] board_player2 = new char[3][3];
+
+        short row = 0;
+        short column = 0;
+
+        //fill the board
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == row && j == column){
+                    board_player1[i][j] = 'X';
+                    board_player2[i][j] = 'X';
+                } else if (i == row && j == column + (short)1) {
+                    board_player2[i][j] = 'O';
+                }
+            }
+        }
+
+        //test player1
+        game1.play(row, column);
+        Assertions.assertEquals(board_player1[row][column], game1.getBoard()[row][column]);
+
+        //test player2
+        game2.play(row, column);
+        column = (short)1;
+        game2.play(row,column);
+        Assertions.assertEquals(board_player2[row][column], game2.getBoard()[row][column]);
     }
 
 }
