cb25064eea7ca813b4034395bdd205863c3853f3
ismam <imehdi@elpuig.xeill.net>
2024-04-27 11:54:24 +0200

    Se ha añadido una nueva comprovacion en el metodo play

--- src/Joc.java
+++ src/Joc.java
@@ -1,7 +1,7 @@
 import jdk.jshell.spi.ExecutionControl;
 
 public class Joc {
-    private char[][] board;
+    private char[][] board = new char[3][3];
     private short turn = 1;
 
     //Methods
@@ -9,18 +9,19 @@ public class Joc {
         throw new ExecutionControl.NotImplementedException("Method newGame not implemented");
     }
     public void play(short row, short column)  {
-        board = new char[3][3];
-
-        //add pice X or O and switch the turns
-        switch (turn) {
-            case 1:
-                board[row][column] = 'X';
-                turn = 2;
-                break;
-            case 2:
-                board[row][column] = 'O';
-                turn = 1;
-                break;
+        //check if this element are empty
+        if (board[row][column] != 'X' && board[row][column] != 'O') {
+            //add pice X or O and switch the turns
+            switch (turn) {
+                case 1:
+                    board[row][column] = 'X';
+                    turn = 2;
+                    break;
+                case 2:
+                    board[row][column] = 'O';
+                    turn = 1;
+                    break;
+            }
         }
     }
     public boolean winning_play(short row,short column) throws ExecutionControl.NotImplementedException {
