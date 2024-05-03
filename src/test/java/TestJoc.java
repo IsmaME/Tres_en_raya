69a37e6791242336c84a33b827478a6f46d8ba96
IsmaME <imehdi@elpuig.xeill.net>
2024-05-03 18:00:50 +0200

    Implementacion de pruebas unitarias con maven

--- src/test/java/TestJoc.java
+++ src/test/java/TestJoc.java
@@ -1,5 +1,7 @@
 import jdk.jshell.spi.ExecutionControl;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class TestJoc {
     @org.junit.jupiter.api.Test
